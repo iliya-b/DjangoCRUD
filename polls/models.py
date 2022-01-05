52386e806ebf2770c30f8bc35965f0b855fdf0bf
Ayili Nikturab <nikturab@outlook.com>
2022-01-05 22:17:11 +0500

    feat: views

--- polls/models.py
+++ polls/models.py
@@ -1,9 +1,16 @@
+import datetime
+import importlib
+
 from django.db import models
+from django.utils import timezone
 
 
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
+
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
 
 
 class Choice(models.Model):
@@ -22,5 +29,3 @@ class Post(models.Model):
     pub_date = models.DateTimeField('date published')
     question = models.ForeignKey(Question, on_delete=models.SET_NULL, null=True)
     tags = models.ManyToManyField(Tag)
-
-
