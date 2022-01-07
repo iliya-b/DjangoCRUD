a4fbb48ef6dc7c14b4e2348e5ad5647491c46bdc
Ayili Nikturab <nikturab@outlook.com>
2022-01-07 19:39:22 +0500

    feat: company model

--- rcoffee/models.py
+++ rcoffee/models.py
@@ -20,6 +20,8 @@ class User(models.Model):
     is_active = models.BooleanField(default=True)
     is_verified = models.BooleanField(default=False)
     is_admin = models.BooleanField(default=False)
+    companies = models.ManyToManyField('Company')
+
     created_at = models.DateTimeField(default=datetime.now)
     updated_at = models.DateTimeField(auto_now=True)
 
@@ -32,6 +34,24 @@ class User(models.Model):
                 f'*Чем занимается:* {self.work}\n'
                 f'*Зацепки для начала разговора:* {self.about}\n\n'
                 f'Напиши собеседнику в Telegram – [{self.name}](tg://user?id={self.telegram_id})')
+
+    def __str__(self):
+        return "User %s (%d)" % (self.name, self.id)
+
+
+class Company(models.Model):
+    name = models.CharField('Name', max_length=100, null=False, unique=True)
+    password = models.CharField(max_length=255, null=False)
+    admin = models.ForeignKey(User, on_delete=models.RESTRICT, null=True)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return "Company %s (%d)" % (self.name, self.id)
+
+    class Meta:
+        verbose_name_plural = 'Companies'
 
 
 class Pair(models.Model):
